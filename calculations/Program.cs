56332c0f5efd4e64d1a87fa386b729b886c16bdf
adilya-g <uitaci099@gmail.com>
2024-11-10 02:10:40 +0300

    conspect added

--- calculations/Program.cs
+++ calculations/Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace calculations // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+			try
+			{
+				Console.WriteLine(CalculateOp(2, 0, '/'));
+			}
+			catch(Exception ex)
+			{
+				Console.WriteLine($"Произошла ошибка! {ex}");
+			}
+        }
+		static double CalculateOp(long a, long b, char operand)
+		{
+			switch(operand)
+			{
+				case '+':
+					return a + b;
+				case '-':
+					return a - b;
+				case '*':
+					return a * b;
+				case '/':
+					/*if (b != 0)
+					{
+						return (double)a / b;
+					}
+					return 0;*/
+					return (double)(a / b);
+				case '^':
+					return Math.Pow(a, b);
+				default:
+					return default(double);
+					
+			}
+		}
+    }
+}
--- types/Program.cs
+++ types/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string name = Console.ReadLine();
+int age = int.Parse(Console.ReadLine());
+int course = int.Parse(Console.ReadLine());
+string groupNum = Console.ReadLine();
+bool hasHomePet = 50 > age;
+
+Console.WriteLine("Анкета студента");
+Console.WriteLine("Вас зовут: "+name);
+Console.WriteLine("Ваш возраст: " + age);
+Console.WriteLine("Ваш курс: " + course);
+Console.WriteLine("Номер группы: " + groupNum);
+Console.WriteLine("Питомец: " + hasHomePet);
+
